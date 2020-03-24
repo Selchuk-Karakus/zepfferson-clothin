09f19c7a9de7fde1c49f2132f5bf116c05a6b59a
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-03-24 21:29:42 +0000

    restructure project architecture

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homePage.component'
+import HomePage from './pages/homePage/homePage.component'
 
 function App() {
   return (
