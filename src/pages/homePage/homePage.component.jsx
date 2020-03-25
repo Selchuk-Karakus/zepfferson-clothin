1db52a7b07b8257bee77540c4b5d0279765e37d6
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-03-25 11:55:17 +0000

    create directory component

--- src/pages/homePage/homePage.component.jsx
+++ src/pages/homePage/homePage.component.jsx
@@ -1,40 +1,10 @@
 import React from 'react';
+import Directory from '../../components/directory/directory.component';
 import './homePage.styles.scss';
 
 const HomePage = () => (
     <div className="homepage">
-        <div className="directory-menu">
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">HATS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">JACKETS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">JEANS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">MENS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">WOMENS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-        </div>
+        <Directory />
     </div>
 );
 
