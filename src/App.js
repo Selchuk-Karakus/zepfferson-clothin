24b83c41e48855c2bba1981b54c9b3ed77dbfe0d
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-04-16 18:33:35 +0100

    update app to redirect to homepage if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import HomePage from "./pages/homePage/homePage.component";
 import ShopPage from "./pages/shop/shop.component";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Header from "./components/header/header-component";
 import SignInAndSignUp from "./pages/shop/sign-in-and-sign-up.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
@@ -42,15 +42,29 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUp} />
+          <Route 
+            exact 
+            path="/signin" 
+            render={() => 
+              this.props.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SignInAndSignUp />
+              )
+            } 
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+});
 
-export default connect(null, mapDispatchToProps )(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,4 +3,4 @@ import userReducer from './user/user.reducer';
 
 export default combineReducers({
     user: userReducer
-})
+});
