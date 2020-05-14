d01a288bbeae6ebad637cfa0e2e101660b29ba33
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-05-14 16:05:32 +0100

    fix bug in collection preview not been rendered properly

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,12 @@
-import React from "react";
-import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../components/preview-collection/collection-preview.component";
+  
+import React from 'react';
+import SHOP_DATA from './shop.data.js';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       collections: SHOP_DATA
     };
@@ -13,9 +15,9 @@ class ShopPage extends React.Component {
   render() {
     const { collections } = this.state;
     return (
-      <div className="shop-page">
-        {collections.map(({ id, ...restCollectionProps }) => (
-          <CollectionPreview key={id} {...restCollectionProps} />
+      <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
         ))}
       </div>
     );
