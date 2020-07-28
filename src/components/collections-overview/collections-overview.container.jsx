94d3396710bb480321ac8844ce5a66213b8a3aae
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-07-28 16:56:49 +0100

    add redux-thunk middleware and switch collections overview and collections page to use HOC container s

---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import { selectIsCollectionsFetching } from '../../redux/shop/shop.selectors';
+import WithSpinner from '../with-spinner/with-spinner.component';
+import CollectionsOverview from './collections-overview.component';
+
+const mapStateToProps = createStructuredSelector({
+    isLoading: selectIsCollectionsFetching
+});
+
+const CollectionsOverviewContainer = compose(
+    connect(mapStateToProps),
+    WithSpinner
+)(CollectionsOverview);
+
+export default CollectionsOverviewContainer;
+
+
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,9 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 import { persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
 
-const middlewares = [];
+const middlewares = [thunk];
 
 if (process.env.NODE_ENV ==='development') {
     middlewares.push(logger);
