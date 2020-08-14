7b93572ca9aa2910679601398323f4bddd10ec04
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-08-14 11:39:30 +0100

    fix binding bug

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -4,12 +4,14 @@ import SignIn from '../../components/sign-in/sign-in.component';
 import SignUp from '../../components/sign-up/sign-up.component';
 
 
-import './sign-in-and-sign-up.styles.scss';
+import { SignInAndSignUpContainer } from './sign-in-and-sign-up.styles';
 
 const SignInAndSignUpPage = () => (
   <div className='sign-in-and-sign-up'>
-    <SignIn />
-    <SignUp />
+    <SignInAndSignUpContainer>
+      <SignIn />
+      <SignUp />
+    </SignInAndSignUpContainer>
   </div>
 );
 
