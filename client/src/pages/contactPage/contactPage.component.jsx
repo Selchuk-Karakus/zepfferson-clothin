c35ffd44482c3fe1a3ce53f39363eeff016fd11c
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-09-16 13:47:12 +0100

    create a footer  and add it to the contact page

--- client/src/pages/contactPage/contactPage.component.jsx
+++ client/src/pages/contactPage/contactPage.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Contact from '../../components/contact/contact.component';
+import Footer from '../../components/footer/footer.component';
 import { ContactPageContainer } from './contactPage.styles';
 
 const ContactPage = () => (
   <ContactPageContainer>
     <Contact />
+    <Footer />
   </ContactPageContainer>
 );
 
