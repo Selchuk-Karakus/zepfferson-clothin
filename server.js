53fb4dce13af860c9708bc1ae3e3f613b159c252
Selchuk Karakus <selchuck.karakus@gmail.com>
2021-02-18 16:44:16 +0000

    update npm packages

--- server.js
+++ server.js
@@ -1,51 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const compression = require('compression')
 const enforce = require('express-sslify');
 
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+if (!(process.env.NODE_ENV === 'production')) require('dotenv').config();
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true}));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
+
 
 if (process.env.NODE_ENV === 'production') {
-  // app.use(enforce.HTTPS({ trustProtoHeader: true }));
-  app.use(express.static(path.join(__dirname, 'client/build')));
+	app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+	app.get('*', function(req, res) {
+		res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+	});
 }
 
 app.listen(port, error => {
-  if (error) throw error;
-  console.log('Server running on port ' + port);
+	if (error) throw error;
 });
 
-app.get('./serviceWorker.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+app.get('/service-worker.js', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
 
 app.post('/payment', (req, res) => {
-  const body = {
-    source: req.body.token.id,
-    amount: req.body.amount,
-    currency: 'usd'
-  };
-
-  stripe.charges.create(body, (stripeErr, stripeRes) => {
-    if (stripeErr) {
-      res.status(500).send({ error: stripeErr });
-    } else {
-      res.status(200).send({ success: stripeRes });
-    }
-  });
-});
+	const body = {
+		source: req.body.token.id,
+		amount: req.body.amount,
+		currency: 'usd'
+	};
+	stripe.charges.create(body, (stripeErr, stripeRes) => {
+		if (stripeErr) {
+			res.status(500).send({ error: stripeErr });
+		} else {
+			res.status(200).send({ success: stripeRes });
+		}
+	})
+})
