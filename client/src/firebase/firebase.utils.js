6e530f2c8d8876cde20fecd82a95277d21e045b0
Selchuk Karakus <selchuck.karakus@gmail.com>
2021-02-19 08:54:32 +0000

    typo, change double quote -> single quote

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
   apiKey: "AIzaSyBDqtxBb5W7IM2MLu0hsCurVXsF_8MWwOU",
